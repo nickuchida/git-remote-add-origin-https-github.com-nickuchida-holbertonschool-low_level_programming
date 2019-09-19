292e0626507f9c58183376ddceba0964d15eb5bc
nickuchida <nicholas.y.uchida@gmail.com>
2019-09-19 20:58:51 +0000

    adding space to last digit sentence

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,14 +10,12 @@
 int main(void)
 {
 	int n;
-	int last;
+	int last = n % 10;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	last = n % 10;
-
-	printf("Last digit of");
+	printf("Last digit of ");
 	if (n > 5)
 	{
 		printf("%d is %d and is greater than 5\n", n, last);
