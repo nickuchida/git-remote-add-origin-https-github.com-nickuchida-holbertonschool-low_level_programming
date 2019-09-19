f6e00dc46c4be62530c9a8aec6097d5ec4862fe6
nickuchida <nicholas.y.uchida@gmail.com>
2019-09-19 19:41:10 +0000

    Adding brackets to 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,10 +16,16 @@ int main(void)
 int last;
 last = n%10;
 if (n > 5)
+{
 printf("Last digit of %d is %d and is greater than 5\n", n, last);
+}
 else if (n == 0)
+{
 printf("Last digit of %d is %d and is 0\n", n, last);
+}
 else if (n < 6 && n != 0)
+{
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+}
   return (0);
 }
