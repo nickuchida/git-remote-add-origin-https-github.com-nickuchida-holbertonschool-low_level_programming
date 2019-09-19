27a335f05ca7afb6c731c5e9cb74ea094cbf0eb6
nickuchida <nicholas.y.uchida@gmail.com>
2019-09-19 21:01:51 +0000

    adding space to sentences

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,18 +15,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	printf("Last digit of ");
+	printf("Last digit of");
 	if (n > 5)
 	{
-		printf("%d is %d and is greater than 5\n", n, last);
+		printf(" %d is %d and is greater than 5\n", n, last);
 	}
 	else if (n == 0)
 	{
-		printf("%d is %d and is 0\n", n, last);
+		printf(" %d is %d and is 0\n", n, last);
 	}
 	else if (n < 6 && n != 0)
 	{
-		printf("%d is %d and is less than 6 and not 0\n", n, last);
+		printf(" %d is %d and is less than 6 and not 0\n", n, last);
 	}
 	return (0);
 }
