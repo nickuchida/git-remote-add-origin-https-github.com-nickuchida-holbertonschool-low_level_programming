cc24d21d7e91e32e657a33853aeda54a307e5b4c
nickuchida <nicholas.y.uchida@gmail.com>
2019-09-19 23:08:44 +0000

    8, two for loops to list hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char h;
+
+	for (h = '0'; h <= '9'; h++)
+		putchar(h);
+	for (h = 'a'; h <= 'z'; h++)
+		putchar(h);
+	putchar('\n');
+	return (0);
+}
