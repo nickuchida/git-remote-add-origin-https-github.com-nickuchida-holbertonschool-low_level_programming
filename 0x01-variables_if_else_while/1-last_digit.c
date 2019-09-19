05e4659a52f749333738c3897bd4ca3e73a1544d
nickuchida <nicholas.y.uchida@gmail.com>
2019-09-19 16:54:17 +0000

    2, putchar alphabet

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,11 +13,13 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+int last;
+last = n%10;
 if (n > 5)
-printf("Last digit of %d is %d and is greater than 5\n", n);
+printf("Last digit of %d is %d and is greater than 5\n", n, last);
 else if (n == 0)
-printf("Last digit of %d is %d and is 0\n", n);
+printf("Last digit of %d is %d and is 0\n", n, last);
 else if (n < 6 && n != 0)
-printf("Last digit of %d is %d and is less than 6 not 0\n", n);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 return (0);
 }
