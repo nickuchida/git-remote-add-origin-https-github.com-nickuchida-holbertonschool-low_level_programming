b0affb546c284a121e387ed6571c043c7e080505
nickuchida <nicholas.y.uchida@gmail.com>
2019-09-19 19:53:11 +0000

    pushed incorrect file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,23 +9,24 @@
 */
 int main(void)
 {
-  int n;
+	int n;
+	int last;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-int last;
-last = n%10;
-if (n > 5)
-{
-printf("Last digit of %d is %d and is greater than 5\n", n, last);
-}
-else if (n == 0)
-{
-printf("Last digit of %d is %d and is 0\n", n, last);
-}
-else if (n < 6 && n != 0)
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
-}
-  return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	last = n % 10;
+	if (n > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	}
+	else if (n == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last);
+	}
+	else if (n < 6 && n != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+	}
+	return (0);
 }
