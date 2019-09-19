f617a4ce435de0d14d32800c67db168393446514
nickuchida <nicholas.y.uchida@gmail.com>
2019-09-19 21:29:46 +0000

    changing how the printf statements are worded and split up

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,17 +16,18 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	last = n % 10;
+	printf("Last digit of %d is ", n);
 	if (n > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+		printf("%d and is greater than 5\n", last);
 	}
 	else if (n == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, last);
+		printf("%d and is 0\n", last);
 	}
 	else if (n < 6 && n != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+		printf("%d and is less than 6 and not 0\n", last);
 	}
 	return (0);
 }
