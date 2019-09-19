ea0217a3be02519db59e0e60772f03816b3055b4
nickuchida <nicholas.y.uchida@gmail.com>
2019-09-19 21:58:23 +0000

    trying if instead of if else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,11 +21,11 @@ int main(void)
 	{
 		printf("%d and is greater than 5\n", last);
 	}
-	else if (n == 0)
+	if (n == 0)
 	{
 		printf("%d and is 0\n", last);
 	}
-	else if (n < 6 && n != 0)
+	if (n < 6 && n != 0)
 	{
 		printf("%d and is less than 6 and not 0\n", last);
 	}
