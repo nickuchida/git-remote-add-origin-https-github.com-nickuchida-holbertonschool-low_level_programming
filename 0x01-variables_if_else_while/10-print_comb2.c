970b72df07decc377aa803abcab6f1902ad42432
nickuchida <nicholas.y.uchida@gmail.com>
2019-09-20 02:22:12 +0000

    for loop in for loop, break loop if both are 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char ch;
+	char ch2;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		for(ch2 = '0'; ch2 <= '9'; ch2++)
+		{
+			putchar(ch);
+			putchar(ch2);
+			if (ch == '9' && ch2 == '9')
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
