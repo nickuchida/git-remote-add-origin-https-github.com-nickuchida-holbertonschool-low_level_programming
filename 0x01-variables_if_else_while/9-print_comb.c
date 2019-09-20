1ca6f82c15176cb07f8464f343ea70c2e772f7b3
nickuchida <nicholas.y.uchida@gmail.com>
2019-09-20 01:16:59 +0000

    9, removing new line

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,14 @@ int main(void)
 	int c;
 	for (c = '0'; c <= '9'; c++)
 	{
-		putchar (c);
-		putchar (',');
-		putchar (' ');
+		if (c < '9')
+		{
+			putchar (c);
+			putchar (',');
+			putchar (' ');
+		}
+		else
+			putchar(c);
 	}
-	putchar ('\n');
 	return (0);
 }
