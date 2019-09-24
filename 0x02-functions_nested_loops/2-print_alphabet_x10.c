29806778a8fcf28ce0f0bdca56c55b1f4eecc960
nickuchida <nicholas.y.uchida@gmail.com>
2019-09-24 18:42:33 +0000

    sorry betty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,9 +11,9 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	while(i < 10)
+	while (i < 10)
 	{
-		for(c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
