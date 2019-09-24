c26ec3b1e4c646bc1dbbd112d001d1210d154ac1
nickuchida <nicholas.y.uchida@gmail.com>
2019-09-24 23:13:04 +0000

    11, while loop before if else statement

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,21 +4,10 @@
 
 void print_to_98(int n)
 {
-	if (n < 98)
+	while (n < 98)
 	{
-		while (n <= 98)
-		{
-			if (n == 98)
-			{
-				printf("%d\n", n);
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
-				n++;
-			}
-		}
+		printf("%d, ", n);
+		n++;
 	}
 	if (n > 98)
 	{
@@ -36,6 +25,6 @@ void print_to_98(int n)
 			}
 		}
 	}
-	if (n == 98)
+	else
 		printf("%d\n", n);
 }
