cef4347cadcf0d9b46454bd979ca5646350eab9d
nickuchida <nicholas.y.uchida@gmail.com>
2019-09-24 21:39:34 +0000

    11, potentially going to make code more efficient

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+				n++;
+			}
+		}
+	}
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+				n--;
+			}
+		}
+	}
+}
