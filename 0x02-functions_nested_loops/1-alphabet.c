10fb41a00714fddf21bee5d964e05519946f3d12
nickuchida <nicholas.y.uchida@gmail.com>
2019-09-24 16:44:32 +0000

    adding header to 1, putchar test

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include <holberton.h>
 
 /**
 * print_alphabet function
 *
-* return 0
+* return void
 */
 void print_alphabet(void)
 {
-	char a;
-	for(a = 'a'; a <= 'z'; a++)
+	char c;
+	for(c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(a);
+		_putchar(c);
 	}
 	_putchar('\n');
 	return 0;
