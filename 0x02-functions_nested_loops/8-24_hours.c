cc9777d8a2f5e5d3608b8471e5c4f1a71b467410
nickuchida <nicholas.y.uchida@gmail.com>
2019-09-25 00:31:18 +0000

    pushing backup of 8 before restarting file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int h1, h2, m1, m2;
+	for (h1 = 0; h1 <= 2; h1++)
+		for (h2 = 0; h2 <= 9; h2++)
+			for (m1 = 0; m1 < 6; m1++)
+				for (m2 = 0; m2 <= 9; m2++)
+				{
+					if (
+						_putchar(h1 + '0');
+						_putchar(h2 + '0');
+						_putchar(':');
+						_putchar(m1 + '0');
+						_putchar(m2 + '0');
+						_putchar('\n');
+				}
+}
