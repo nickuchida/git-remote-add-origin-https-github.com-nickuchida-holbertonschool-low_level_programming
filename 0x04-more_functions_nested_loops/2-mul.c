924b2949836446d23ae025497bb486f2d81393f3
nickuchida <nicholas.y.uchida@gmail.com>
2019-09-26 16:20:54 +0000

    task 2, also updating header

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: r
+ */
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+	return (r);
+}
