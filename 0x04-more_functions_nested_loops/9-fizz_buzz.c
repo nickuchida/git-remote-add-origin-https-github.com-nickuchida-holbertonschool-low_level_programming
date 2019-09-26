6e66fa96a1df55cb67400e9b66462bccfd90b50c
nickuchida <nicholas.y.uchida@gmail.com>
2019-09-26 19:06:42 +0000

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * main - prints 1 to 100, prints fizz for multiples of 3 and 5
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0)
+			printf("Fizz");
+		if (n % 5 == 0)
+			printf("Buzz");
+		if (n % 3 != 0 && n % 5 != 0)
+			printf("%d", n);
+		printf(" ");
+	}
+}
