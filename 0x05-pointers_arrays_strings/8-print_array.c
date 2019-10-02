3177ea1606e628453230b6770d33c8696dab3322
nickuchida <nicholas.y.uchida@gmail.com>
2019-10-02 17:59:21 +0000

    task 8 from pld

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_array - prints elements of an array of integers
+ * @a: the array
+ * @n: number of elements in array
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i + 1 != n)
+			printf(", ");
+	}
+	printf("\n");
+}
