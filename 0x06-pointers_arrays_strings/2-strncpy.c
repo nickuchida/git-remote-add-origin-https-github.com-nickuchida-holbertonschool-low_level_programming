9ba490f9d9ba6381ca91c02adf8b3d607b415b71
nickuchida <nicholas.y.uchida@gmail.com>
2019-10-03 17:57:21 +0000

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * *_strncpy - copies a string
+ * @dest: second string
+ * @src: first string
+ * @n: size
+ * Return: copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
