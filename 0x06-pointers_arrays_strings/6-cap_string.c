0640384be1492c84a2777550601f867f74b31ffd
nickuchida <nicholas.y.uchida@gmail.com>
2019-10-04 03:08:36 +0000

    adding edge case for first letter in 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,12 +12,17 @@ char *cap_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; c[j] != '\0'; j++)
+		if (s[0] >= 'a' && s[0] <= 'z')
+			s[0] -= 32;
+		else
 		{
-			if (s[i] == c[j])
+			for (j = 0; c[j] != '\0'; j++)
 			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-					s[i + 1] = s[i + 1] - 32;
+				if (s[i] == c[j])
+				{
+					if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+						s[i + 1] = s[i + 1] - 32;
+				}
 			}
 		}
 	}
