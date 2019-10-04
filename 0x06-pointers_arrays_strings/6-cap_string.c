f983a121ec0581c44771255fdc19e8d5cb853e5c
nickuchida <nicholas.y.uchida@gmail.com>
2019-10-04 00:30:54 +0000

    task 6, fixing the error of skipping the hello after comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,15 +7,18 @@
  */
 char *cap_string(char *s)
 {
-	int i;
+	int i, j;
+	char c[] = " \t\n,;.!?\"(){}";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == ' ' || s[i] == '.' || s[i] == '\t')
+		for (j = 0; c[j] != '\0'; j++)
 		{
-			i++;
-			if (s[i] >= 'a' && s[i] <= 'z')
-				s[i] = s[i] - 32;
+			if (s[i] == c[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] = s[i + 1] - 32;
+			}
 		}
 	}
 	return (s);
