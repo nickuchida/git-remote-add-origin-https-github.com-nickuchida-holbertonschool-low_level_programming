ac9a244d27e64986fe6820f83344f538b5f3388f
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-07 21:26:25 +0000

    3, adding breaks to break out of loop

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string
+ * @accept: substring
+ * Return: num
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	unsigned int length;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				length++;
+				break;
+			}
+		}
+		if (!accept[j])
+			break;
+	}
+	return (length);
+}
