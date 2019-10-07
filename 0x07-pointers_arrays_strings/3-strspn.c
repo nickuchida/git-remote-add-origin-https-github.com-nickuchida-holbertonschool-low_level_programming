a436369f92f264db2bf2aeb1d64e0902fb1e3813
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-07 21:49:51 +0000

    assigning value 0 to starting length

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,7 +9,7 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j;
-	int length;
+	int length = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -21,7 +21,7 @@ unsigned int _strspn(char *s, char *accept)
 				break;
 			}
 		}
-		if (!accept[j])
+		if (accept[j] == '\0')
 			break;
 	}
 	return (length);
