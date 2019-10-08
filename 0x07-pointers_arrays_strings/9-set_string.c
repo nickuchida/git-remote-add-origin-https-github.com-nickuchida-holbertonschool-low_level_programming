40c34d9e0dbfda6b80ceaa5f5b20a6fe5e695b9a
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-08 20:02:12 +0000

    pld

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets value of a pointer to a char
+ * @s: pointer to change
+ * @to: change to
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*to = s;
+}
