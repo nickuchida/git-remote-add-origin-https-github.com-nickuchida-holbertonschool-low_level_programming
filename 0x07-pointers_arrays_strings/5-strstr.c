6d8d5812b06623096c75ef89692f8967a0cc397b
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-08 23:29:59 +0000

    pld

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,13 +8,19 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack && *haystack != *needle)
-	{
-		haystack++;
-		if (*haystack == *needle)
-			return (haystack);
-	}
-	if (*needle == '\0')
+	int i, j;
+
+	if (needle == '\0')
 		return (haystack);
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+			if (needle[j + 1] == '\0')
+				return (haystack + i);
+		}
+	}
 	return (0);
 }
