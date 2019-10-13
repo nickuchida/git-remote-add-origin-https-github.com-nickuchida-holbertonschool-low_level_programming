4f56d33026ab43e8bc65b25db5f650ee45a845d3
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-13 19:44:16 +0000

    task 2, betty style

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
 
 /**
  * main - prints all arguments received
+ * @argc: number of arguments
+ * @argv: name of arguments
  * Return: result
  */
 int main(int argc, char *argv[])
