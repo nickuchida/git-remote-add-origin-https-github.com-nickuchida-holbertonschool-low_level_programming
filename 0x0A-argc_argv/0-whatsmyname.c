81f6ddb4dbb4920a52a084e7df8e2645deaa9558
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-13 19:42:07 +0000

    betty style for task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
 
 /**
  * main - prints the name of program followed by a new line
+ * @argc: number of arguments
+ * @argv: name of arguments
  * Return: name of program
  */
 int main(int argc __attribute__((unused)), char *argv[])
