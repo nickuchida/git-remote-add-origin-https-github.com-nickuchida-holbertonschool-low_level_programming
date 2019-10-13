ac30941126b7278470f6d30a69c3b20313f4455d
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-13 20:59:15 +0000

    changing conditions for error message in task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,15 +12,15 @@ int main(int argc, char *argv[])
 {
 	int result;
 
-	if (argc < 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
+	if (argc == 3)
 	{
 		result = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", result);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 	return (0);
 }
