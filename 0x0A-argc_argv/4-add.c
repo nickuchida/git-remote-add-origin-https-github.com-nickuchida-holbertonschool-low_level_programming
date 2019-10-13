2cd1c431e6a1ddfe128d66bdc25373c65d8e272e
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-13 21:26:46 +0000

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,22 +1,40 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
  * main - adds positive numbers
+ * @argc: number of arguments
+ * @argv: numbers being added
  * Return: result
  */
 int main(int argc, char *argv[])
 {
 	int i, sum = 0;
+	unsigned int j, length;
+	char *s;
 
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
+			s = argv[i];
+			length = strlen(s);
+			for (j = 0; j < length; j++)
+			{
+				if (isdigit(*(s + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
 			sum += atoi(argv[i]);
 		}
 		printf("%d\n", sum);
 	}
+	else
+		printf("0\n");
 	return (0);
 }
