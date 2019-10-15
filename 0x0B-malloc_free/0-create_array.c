7ac7126ad4d5743eeba119caddbf5d59441324aa
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-15 20:51:17 +0000

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *create_array - creates an array of chars
+ * @size: size of array
+ * @c: character initialized
+ * Return: arr
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	if (sizeof(size) == 0)
+		return (NULL);
+	arr = malloc(sizeof(c) * size);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+	arr[i] = '\0';
+	return (arr);
+}
