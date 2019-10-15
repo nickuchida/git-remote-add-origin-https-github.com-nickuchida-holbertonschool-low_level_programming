296b6232a182e312ed603b77ebd700e5d5d29ba2
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-15 21:49:07 +0000

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *_strdup - returns a pointer to a newly allocated space in memory
+ * @str: string
+ * Return: copy
+ */
+char *_strdup(char *str)
+{
+	int i;
+	int length = 0;
+	char *copy;
+
+	while (str[length])
+		length++;
+	copy = malloc(length + 1);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < length; i++)
+		copy[i] = str[i];
+	copy[i] = '\0';
+	return (copy);
+}
