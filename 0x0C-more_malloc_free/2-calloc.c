14763ad91e47872de3237a9265271eba38808b4c
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-17 22:45:11 +0000

    change arr = 0 to NULL

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	arr = malloc(nmemb * size);
-	if (arr == 0)
+	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
 		arr[i] = 0;
