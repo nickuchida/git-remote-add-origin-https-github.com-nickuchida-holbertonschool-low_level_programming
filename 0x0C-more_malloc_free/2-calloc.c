92bba85f7a38170a3e8d3b89b1e63cd5eab4dcb6
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-17 22:43:20 +0000

    removing size of because size is provided

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	arr = malloc(sizeof(int) * (nmemb * size));
+	arr = malloc(nmemb * size);
 	if (arr == 0)
 		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
