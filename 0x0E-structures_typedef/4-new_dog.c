e92ae3a07106b9bbb880823230d9f9d65f567657
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-21 22:27:01 +0000

    task 4, create a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *my_dog;
+
+	my_dog = malloc(sizeof(dog_t));
+	if (my_dog == NULL)
+		return (NULL);
+	my_dog->name = name;
+	my_dog->age = age;
+	my_dog->owner = owner;
+	return (my_dog);
+}
