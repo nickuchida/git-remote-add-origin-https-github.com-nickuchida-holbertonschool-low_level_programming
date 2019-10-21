be342ec65fda1ea729f4d768180956b442d2b248
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-21 23:14:54 +0000

    4, adding space for stored copy of name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,20 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - find length of string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
  * *new_dog - creates a new dog
  * @name: name of dog
  * @age: age of dog
@@ -16,6 +30,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
 		return (NULL);
+	malloc(sizeof(char) * _strlen(name));
+	malloc(sizeof(char) * _strlen(owner));
+
 	my_dog->name = name;
 	my_dog->age = age;
 	my_dog->owner = owner;
