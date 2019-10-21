f3c7647f9885d953ffd574c87e4f9556d2fdaecc
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-21 22:00:54 +0000

    NULL and nil conditions

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,6 +11,12 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	if (d->age == 0)
+		printf("Age: (nil)\n");
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
