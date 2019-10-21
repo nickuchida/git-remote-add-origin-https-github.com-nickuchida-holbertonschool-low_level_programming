8beb37db93789cce1bda60db202de71c9b6d097b
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-21 21:51:58 +0000

    check for null structure

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * init_dog - initiates a dog
@@ -10,6 +11,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return ;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
