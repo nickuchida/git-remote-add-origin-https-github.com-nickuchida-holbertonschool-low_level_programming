d3b35abc4956f0f17fb52d1df3efa478075fb307
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-21 22:40:07 +0000

    adding description for main function

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,12 @@
 #include "holberton.h"
 #include <stdio.h>
 
+/**
+ * main - print name of source file
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0
+ */
 int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
 {
 	printf("%s\n", __FILE__);
