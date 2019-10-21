cf10779ff23b2f0fa63be7ac4759c6bc714bc28b
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-21 19:40:36 +0000

    betty formatting for structs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - descriptions of dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ *
+ * Description: this structure contains different stats of different dogs
+ */
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
 
 #endif
