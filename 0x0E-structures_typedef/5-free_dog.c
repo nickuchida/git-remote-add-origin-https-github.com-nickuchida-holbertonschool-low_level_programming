93753912588184a5be39dc1e8e01da65f97474fb
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-22 20:25:30 +0000

    task 5, free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - free the dogs
+ * @d: structure dog
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
