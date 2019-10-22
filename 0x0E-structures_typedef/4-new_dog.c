83ef96dc552321384a57f0dc987d8b4f911d6348
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-22 04:40:35 +0000

    4, adding plus 1 to malloc

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -51,18 +51,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (my_dog == NULL)
 		return (NULL);
 
-	copy1 = malloc(sizeof(char) * _strlen(name));
+	copy1 = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (copy1 == NULL)
 		return (NULL);
-	copy2 = malloc(sizeof(char) * _strlen(owner));
+	copy2 = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (copy2 == NULL)
 		return (NULL);
 
 	_strcpy(copy1, name);
-	my_dog->name = copy1 + 1;
+	my_dog->name = copy1;
 	my_dog->age = age;
 	_strcpy(copy2, owner);
-	my_dog->owner = copy2 + 1;
+	my_dog->owner = copy2;
 
 	return (my_dog);
 }
