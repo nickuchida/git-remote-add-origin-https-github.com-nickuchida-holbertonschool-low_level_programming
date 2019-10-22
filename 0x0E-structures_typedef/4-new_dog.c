52fa6e1a664e3780da10f4d4aedc1d267235d1f7
Nick Uchida <nicholas.y.uchida@gmail.com>
2019-10-22 04:38:27 +0000

    adding strcpy function

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,6 +17,24 @@ int _strlen(char *s)
 }
 
 /**
+ * *_strcpy - copies a string
+ * @dest: destination
+ * @src: source
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *p = dest;
+
+	while ((*p = *src) != '\0')
+	{
+		p++;
+		src++;
+	}
+	return (dest);
+}
+
+/**
  * *new_dog - creates a new dog
  * @name: name of dog
  * @age: age of dog
@@ -30,16 +48,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *copy2;
 
 	my_dog = malloc(sizeof(dog_t));
-
-	copy1 = malloc(sizeof(char) * _strlen(name));
-	copy2 = malloc(sizeof(char) * _strlen(owner));
-
-	copy1 = name;
-	my_dog->name = copy1;
-	my_dog->age = age;
-	copy2 = owner;
-	my_dog->owner = copy2;
 	if (my_dog == NULL)
 		return (NULL);
+
+	copy1 = malloc(sizeof(char) * _strlen(name));
+	if (copy1 == NULL)
+		return (NULL);
+	copy2 = malloc(sizeof(char) * _strlen(owner));
+	if (copy2 == NULL)
+		return (NULL);
+
+	_strcpy(copy1, name);
+	my_dog->name = copy1 + 1;
+	my_dog->age = age;
+	_strcpy(copy2, owner);
+	my_dog->owner = copy2 + 1;
+
 	return (my_dog);
 }
