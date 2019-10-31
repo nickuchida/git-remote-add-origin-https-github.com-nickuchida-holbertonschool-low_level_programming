3d455a3bf8fc0b2d0a96dd9c2c133cd0e4a12cae
nickuchida <nicholas.y.uchida@gmail.com>
2019-10-31 20:59:44 +0000

    seeing if on the right track

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,24 @@
 #include "lists.h"
 
+/**
+ * _strlen - returns the length of a string
+ * @s: the character string being measured
+ * Return: i
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
 size_t print_list(const list_t *h)
 {
-	if (str == NULL)
+	if (h->str == NULL)
 		printf("[0] (nil)\n");
+	printf("[%d] %s", _strlen(h->str), h->str);
+	printf("\n");
+	return (0);
 }
