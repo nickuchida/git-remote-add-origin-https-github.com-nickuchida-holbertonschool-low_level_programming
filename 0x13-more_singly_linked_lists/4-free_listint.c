a766f101e1c40a1999f31bd9a2f297cf9715d2db
nickuchida <nicholas.y.uchida@gmail.com>
2019-11-06 00:48:34 +0000

    adding description to task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,6 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * free_listint - frees a linked list
+ * @head: the list
+ * Return: void
+ */
 void free_listint(listint_t *head)
 {
 	listint_t *tmp;
