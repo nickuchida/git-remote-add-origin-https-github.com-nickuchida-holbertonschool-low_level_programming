05cda043394ae7ea8cf439ccf21c1ebe380fa7d5
nickuchida <nicholas.y.uchida@gmail.com>
2019-11-06 00:01:46 +0000

    betty style for task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * listint_len - return number of elements in list
+ * @h: the list
+ * Return: number of elements
+ */
 size_t listint_len(const listint_t *h)
 {
 	size_t element = 0;
