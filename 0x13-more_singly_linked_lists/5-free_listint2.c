3c40c17890d3c7ee6c22c35677da13e7b114801d
nickuchida <nicholas.y.uchida@gmail.com>
2019-11-06 00:54:52 +0000

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: the list
+ * Return: void
+ */
+void free_listint2(listint_t *head)
+{
+	free_listint(*head);
+	*head = NULL;
+}
