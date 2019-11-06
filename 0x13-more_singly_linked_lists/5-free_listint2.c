17443964b3ae418e3cadf43e6dedad474b8c91c8
nickuchida <nicholas.y.uchida@gmail.com>
2019-11-06 04:39:51 +0000

    adding freelist function above main function

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,6 +4,23 @@
 #include <stdlib.h>
 
 /**
+ * free_listint - frees a linked list
+ * @head: the list
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+
+/**
  * free_listint2 - frees a linked list
  * @head: the list
  * Return: void
