ec111d12792ae8c03a9ccfb19af2cfb34ce0b3a5
nickuchida <nicholas.y.uchida@gmail.com>
2019-11-08 18:25:43 +0000

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_bit - returns value of a bit at an index
+ * @n: the number to be checked
+ * @index: location of the bit
+ * Return: value of bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask = 1;
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	mask = mask << index;
+	if ((n & mask) == 0)
+		return (0);
+	else
+		return (1);
+}
