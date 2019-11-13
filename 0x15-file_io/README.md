5f85780ae50678c499cd6497cb147503cd7720e7
nickuchida <nicholas.y.uchida@gmail.com>
2019-11-13 03:39:11 +0000

    header and read me file

---
+++ 0x15-file_io/README.md
@@ -0,0 +1,4 @@
+#File I/O
+This project is about File I/O.
+It uses the I/O system calls open, close, read, and write.
+How to use the flags 0_RDONLY, 0_WRONLY, and 0_RDWR.
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,13 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif _HOLBERTON_H_
