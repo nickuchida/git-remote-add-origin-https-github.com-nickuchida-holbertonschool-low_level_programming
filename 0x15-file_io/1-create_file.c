6bee8a15bf55a1d86a02873562763c69ccd535ff
nickuchida <nicholas.y.uchida@gmail.com>
2019-11-13 23:57:12 +0000

    task 1, adding write command and counting for size of string. return write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,8 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, wc, count;
+	char *buffer [1024];
 
 	if (filename == NULL)
 		return (-1);
@@ -18,5 +19,13 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT, 0600);
 	if (fd == -1)
 		return (0);
-	return (fd);
+
+	for (count = 0; text_content[count] != '\0'; count++)
+		;
+
+	wc = write(fd, buffer, count);
+	if (wc == -1)
+		return (0);
+
+	return (wc);
 }
