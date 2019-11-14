942cac72357ba94b5274ba228d320c9f88938593
nickuchida <nicholas.y.uchida@gmail.com>
2019-11-14 00:32:53 +0000

    changing return value of failure

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,25 +9,24 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, wc, count;
-	char *buffer [1024];
+	char *buffer[1024];
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
 
-	fd = open(filename, O_RDWR | O_CREAT, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
-		return (0);
+		return (-1);
 
 	for (count = 0; text_content[count] != '\0'; count++)
 		;
 
 	wc = write(fd, buffer, count);
 	if (wc == -1)
-		return (0);
+		return (-1);
+	close(fd);
 
-	if (close(fd))
-		return (0);
 	return (1);
 }
