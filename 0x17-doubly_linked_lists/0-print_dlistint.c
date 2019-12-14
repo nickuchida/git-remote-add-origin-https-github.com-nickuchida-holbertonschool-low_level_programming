2c2c031e0f1d994f93a20ba7b945920c630afaee
nickuchida <nicholas.y.uchida@gmail.com>
2019-12-14 20:38:17 +0000

    betty format

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - print linked list
+ * @h: head
+ * Return: number of elements
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t element = 0;
