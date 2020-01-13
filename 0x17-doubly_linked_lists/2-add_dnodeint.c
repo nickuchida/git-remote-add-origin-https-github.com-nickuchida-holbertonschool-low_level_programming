e894f9bf19390e13a3bec65319e27ab65f4a8b81
nickuchida <nicholas.y.uchida@gmail.com>
2020-01-13 22:30:07 +0000

    changing return value and setting head to new

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,18 +10,19 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	if (!head || !n)
-		return (0);
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
 	new->n = n;
-	if (!new->n)
+	new->prev = NULL;
+
+	if (*head == NULL)
 	{
-		free(new);
-		return (NULL);
+		*head = new;
+		return (*head);
 	}
 	new->next = *head;
+	(*head)->prev = new;
 	*head = new;
-	return (new);
+	return (*head);
 }
